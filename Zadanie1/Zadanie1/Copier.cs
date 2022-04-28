373cb5b2732c2dcee7e9cb853dc4ca5a6306a9b0
Piotr <piotr.zywczak@microsoft.wsei.edu.pl>
2022-04-28 16:47:42 +0200

    property

---
+++ Zadanie1/Zadanie1/Copier.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ver1
+{
+	public class Copier : BaseDevice, IPrinter, IScanner
+	{
+		public int PrintCounter { get; set; } = 0;
+		public int ScanCounter { get; set; } = 0;
+		public int Counter { get; set; }
+
+
+	}
+
+}
