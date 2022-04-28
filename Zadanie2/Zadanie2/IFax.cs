898c9b285ea75060b3e8d1667c87ee386d505bc1
Piotr <piotr.zywczak@microsoft.wsei.edu.pl>
2022-04-28 18:14:49 +0200

    zad2

---
+++ Zadanie2/Zadanie2/IFax.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zadanie2
+{
+    public interface IFax : IDevice
+    {
+        public void Fax(IDocument document, string faxNumber);
+    }
+}
---
+++ Zadanie2/Zadanie2/MultifunctionalDevice.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zadanie2
+{
+	 public class MultifunctionalDevice :BaseDevice, IPrinter, IScanner, IFax
+    {
+		
+		public int PrintCounter { get; set; } = 0;
+		public int ScanCounter { get; set; } = 0;
+		public new int Counter { get; set; }
+		public string FaxNumber { get; set }
+
+		public MultifunctionalDevice(string faxNumber)
+		{
+			FaxNumber = faxNumber;
+		}
+
+		public void Print(in IDocument document)
+		{
+			if (GetState() == IDevice.State.off)
+				return;
+
+			if (state == IDevice.State.on)
+			{
+				Console.WriteLine("{0} Print: {1}", DateTime.Now, document.GetFileName());
+				PrintCounter++;
+			}
+		}
+
+		public void PowerOn()
+		{
+			if (GetState() == IDevice.State.off)
+				Counter++;
+
+			base.PowerOn();
+		}
+
+
+		public void Scan(out IDocument document, IDocument.FormatType formatType = IDocument.FormatType.JPG)
+		{
+			string type = "";
+
+
+			switch (formatType)
+			{
+				case IDocument.FormatType.TXT:
+					type = "Text";
+					break;
+				case IDocument.FormatType.PDF:
+					type = "PDF";
+					break;
+				case IDocument.FormatType.JPG:
+					type = "Image";
+					break;
+				default:
+					throw new Exception("Invalid type!");
+
+			}
+
+			string nameDocument = string.Format($"{type}{ScanCounter + 1}.{formatType.ToString().ToLower()}");
+
+			if (formatType == IDocument.FormatType.PDF)
+			{
+				document = new PDFDocument(nameDocument);
+			}
+			else if (formatType == IDocument.FormatType.TXT)
+			{
+				document = new TextDocument(nameDocument);				}
+			else
+			{
+				document = new ImageDocument(nameDocument);				}
+
+			if (state == IDevice.State.on)
+			{
+				ScanCounter++;
+				Console.WriteLine("{0}, Scan: {1}", DateTime.Now, document.GetFileName());
+			}
+
+			}
+		public void Fax(IDocument document, string faxNumber)
+		{
+			if (state == IDevice.State.on)
+			{
+				Counter++;
+				Console.WriteLine($"{DateTime.Today} Sent: {document.GetFileName()} from: {this.FaxNumber} to: {faxNumber}");
+			}
+		}
+
+		public void ScanAndSend(string faxNumber)
+		{
+			Scan(out IDocument doc);
+			Fax(doc, faxNumber);
+		}
+		public void ScanAndPrint()
+			{
+				Scan(out IDocument document, IDocument.FormatType.JPG);
+				Print(document);
+			}
+		}
+	}
+
