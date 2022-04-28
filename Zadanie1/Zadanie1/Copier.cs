f6a5c73afc7367d9672b0acf72d0673907423fea
Piotr <piotr.zywczak@microsoft.wsei.edu.pl>
2022-04-28 17:24:56 +0200

    method scan

--- Zadanie1/Zadanie1/Copier.cs
+++ Zadanie1/Zadanie1/Copier.cs
@@ -6,12 +6,74 @@ namespace ver1
 	{
 		public int PrintCounter { get; set; } = 0;
 		public int ScanCounter { get; set; } = 0;
-		public int Counter { get; set; }
+		public new int Counter { get; set; }
 
 		public void Print(in IDocument document)
 		{
+			if (GetState() == IDevice.State.off)
+				return;
+		
 			if (state == IDevice.State.on)
+			{
 				Console.WriteLine("{0} Print: {1}", DateTime.Today, document.GetFileName());
+				PrintCounter++;
+			}
+		}
+
+		public void PowerOn()
+		{
+			if (GetState() == IDevice.State.off)
+				Counter++;
+
+			base.PowerOn();
+		}
+		
+
+		public void Scan(out IDocument document, IDocument.FormatType formatType = IDocument.FormatType.JPG)
+		{
+			string type = "";
+			
+
+			switch (formatType)
+			{
+				case IDocument.FormatType.TXT:
+					type = "Text";
+					break;
+				case IDocument.FormatType.PDF:
+					type = "PDF";
+					break;
+				case IDocument.FormatType.JPG:
+					type = "Image";
+					break;
+				default:
+					throw new Exception("Invalid type!");
+
+			}
+
+			string nameDocument = string.Format($"{type}{ScanCounter + 1}.{formatType.ToString().ToLower()}");
+
+			if (formatType == IDocument.FormatType.PDF)
+			{
+				document = new PDFDocument(nameDocument);
+			}
+			else if (formatType == IDocument.FormatType.TXT)
+			{
+				document = new TextDocument(nameDocument);
+			}
+			else
+			{
+				document = new ImageDocument(nameDocument);
+			}
+
+			if (state == IDevice.State.on)
+			{
+				ScanCounter++;
+				Console.WriteLine("{0}, Scan: {1}",DateTime.Today, document.GetFileName());
+			}
+		
+
+
+
 		}
 	}
 
